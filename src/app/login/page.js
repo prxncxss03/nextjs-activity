e97e656333c852c60422385db577a6ad00112889
prxncxss03 <pocon.evangeline@gmail.com>
2024-10-11 00:48:48 +0800

    fix: add image placeholder

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,10 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
 import { signup } from "./action";
 import Image from "next/image";
-import { ArrowLeftIcon } from "@radix-ui/react-icons"; 
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -13,20 +13,19 @@ export default function LoginPage() {
     <div className="flex h-screen items-center justify-center bg-gradient-to-b from-gray-100 to-gray-200">
       {/* Back Button */}
       <Button
-          variant="ghost"
-          className="absolute top-4 left-4 flex items-center space-x-2 text-gray-600 hover:text-gray-900"
-          onClick={() => router.push("/")}
-        >
-          <ArrowLeftIcon className="h-4 w-4" />
-          <span>Back</span>
-        </Button>
+        variant="ghost"
+        className="absolute top-4 left-4 flex items-center space-x-2 text-gray-600 hover:text-gray-900"
+        onClick={() => router.push("/")}
+      >
+        <ArrowLeftIcon className="h-4 w-4" />
+        <span>Back</span>
+      </Button>
       <div className="p-8 bg-white shadow-lg rounded-xl w-full max-w-sm relative">
-        
-        
-        
         {/* Title */}
-        <h1 className="text-2xl font-semibold text-center mb-6 text-gray-800">Sign in to Your Account</h1>
-        
+        <h1 className="text-2xl font-semibold text-center mb-6 text-gray-800">
+          Sign in to Your Account
+        </h1>
+
         {/* Form */}
         <form className="flex flex-col items-center">
           <Button
@@ -35,7 +34,12 @@ export default function LoginPage() {
             className="w-full bg-white text-gray-700 border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-blue-500 focus:outline-none flex items-center justify-center space-x-3 py-3 px-4 rounded-lg"
           >
             {/* Google Logo */}
-            <Image src="/images/logo/google-logo.png" alt="Google Logo" width={24} height={24} />
+            <Image
+              src="/images/logo/google-logo.png"
+              alt="Google Logo"
+              width={24}
+              height={24}
+            />
             <span className="text-gray-700">Continue with Google</span>
           </Button>
         </form>
--- src/app/page.js
+++ src/app/page.js
@@ -15,12 +15,14 @@ import Activities from "../data/activities.json";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
 import { verify, logout } from "./activity/action";
 import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton"
 
 import UserProfile from "@/components/UserProfile";
 
 export default function Home() {
   const router = useRouter();
   const [user, setUser] = useState(null);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -50,7 +52,7 @@ export default function Home() {
       </header>
 
       {/* Title */}
-      <h1 className="text-4xl font-extrabold text-gray-900 mb-6">Activities</h1>
+      <h1 className="text-4xl font-extrabold text-gray-900 mb-6">🚀 Activities</h1>
 
       {/* Activities Section */}
       <div className="flex flex-wrap justify-center gap-2 w-full max-w-5xl">
@@ -69,13 +71,18 @@ export default function Home() {
                 </CardDescription>
               </CardHeader>
               <CardContent className="flex justify-center items-center">
-                <Image
+                
+            
+                    <Image
                   src={activity.image}
                   alt={activity.name}
                   width={310}
                   height={150}
                   className="rounded-lg select-none"
+                  placeholder = 'blur' 
+                  blurDataURL = {activity.image}
                 />
+                  
               </CardContent>
               <CardFooter className="flex justify-center">
                 <Button
